0dedffc343e2a2051d141e76ad0a3de61c6f308b
harjapdhaliwal <hda36@sfu.ca>
2021-10-03 22:23:52 -0700

    Worked on Activity2 interface

--- app/src/main/java/ca/cmpt276/androidgamescoreapp/CreateGame.java
+++ app/src/main/java/ca/cmpt276/androidgamescoreapp/CreateGame.java
@@ -9,9 +9,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateGame extends AppCompatActivity {
     private GameManager mainManager;
+    int player1Cards, player1Points, player1Wagers, player1Score;
+    int player2Cards, player2Points, player2Wagers, player2Score;
+    EditText player1CardsInput, player1PointsInput, player1WagersInput;
+    EditText player2CardsInput, player2PointsInput, player2WagersInput;
+
+    Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +29,6 @@ public class CreateGame extends AppCompatActivity {
         setSupportActionBar(toolbar2);
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
-
         // Makes the actionbar back button return to the last activity on call stack instead of a new activity
         toolbar2.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,7 +37,26 @@ public class CreateGame extends AppCompatActivity {
             }
         });
 
+
         mainManager = GameManager.getInstance();
+        player1CardsInput = (EditText) findViewById(R.id.player1_cards);
+        player1PointsInput = (EditText) findViewById(R.id.player1_points);
+        player1WagersInput = (EditText) findViewById(R.id.player1_wagers);
+
+        player2CardsInput = (EditText) findViewById(R.id.player2_cards);
+        player2PointsInput = (EditText) findViewById(R.id.player2_points);
+        player2WagersInput = (EditText) findViewById(R.id.player2_wagers);
+
+        saveButton = (Button) findViewById(R.id.save_game);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
 
     }
 
